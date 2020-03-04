9a921f2c6507914c0302e943c7a0784caa35de95
Christophere Johnson <cdjohnson0195@gmail.com>
2020-03-03 22:17:55 -0500

    Added Core project for domain objects.

---
+++ src/MailingListApplication.Core/Entities/Contact.cs
@@ -0,0 +1,12 @@
+﻿using MailingListApplication.SharedKernal;
+
+
+namespace MailingListApplication.Core.Entities
+{
+    class Contact : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
