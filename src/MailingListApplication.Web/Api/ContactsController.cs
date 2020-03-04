091c4074e80af4f346bcb409ea0e6a6e0e893e9e
Christophere Johnson <cdjohnson0195@gmail.com>
2020-03-04 02:23:04 -0500

    Added filter params for api/contacts.

--- src/MailingListApplication.Web/Api/ContactsController.cs
+++ src/MailingListApplication.Web/Api/ContactsController.cs
@@ -14,13 +14,33 @@ namespace MailingListApplication.Web.Api
         {
             _repository = repository;
         }
-        
+
         // GET: api/contacts
         [HttpGet]
-        public IActionResult List()
+        public IActionResult List(string lastName = null, string orderBy = null)
         {
+            // alway sort by asc
             var items = _repository.List<Contact>()
-                .Select(ContactDTO.FromContact);
+                    .OrderBy(x => x.LastName)
+                    .ThenBy(x => x.FirstName)
+                    .Select(ContactDTO.FromContact);
+
+            // only filter the collection if lastName param was supplied
+            if (!string.IsNullOrEmpty(lastName))
+            {
+                items = items.Where(x => x.LastName.ToLower() == lastName.ToLower());
+            }
+
+
+            // only filter the collection if orderBy param was supplied and is Descending
+            if (!string.IsNullOrEmpty(orderBy))
+            {
+                if(orderBy == "Descending")
+                {
+                    items = items.OrderByDescending(x => x.LastName).ThenByDescending(x => x.FirstName);
+                }
+            }
+
             return Ok(items);
         }
     }
